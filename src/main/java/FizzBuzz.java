98323754a49b3a22853edf975bad96546e7d403b
AmirArani <blackstar.m77@gmail.com>
2022-03-10 13:43:58 +0330

    1400/12/19 | clean up

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,15 +1,6 @@
 public class FizzBuzz {
 
     public String play(int adad) {
-//        String result = String.valueOf(adad);
-//        if (ayaBar_3_BakhshPazirHast(adad) && !ayaBar_5_BakhshPazirHast(adad)) {
-//            result = "Fizz";
-//        } else if (ayaBar_5_BakhshPazirHast(adad) && !ayaBar_3_BakhshPazirHast(adad)) {
-//            result = "Buzz";
-//        } else if (ayaBar_5_BakhshPazirHast(adad) && ayaBar_3_BakhshPazirHast(adad)) {
-//            result = "FizzBuzz";
-//        }
-//        return result;
 
         if (adad % 3 == 0){
             if (adad % 5 == 0){
@@ -19,16 +10,8 @@ public class FizzBuzz {
         }else if (adad % 5 == 0){
             return "Buzz";
         }
+
         return String.valueOf(adad);
-
-    }
-
-    private boolean ayaBar_5_BakhshPazirHast(int adad) {
-        return adad % 5 == 0;
-    }
-
-    private boolean ayaBar_3_BakhshPazirHast(int adad) {
-        return adad % 3 == 0;
     }
 
 }
